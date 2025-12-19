775a745507e0be8e065667944ed3f3e20824ef48
r0rnor <r0rnor@dev.com>
2025-12-19 19:56:48 +0100

    refactor(ui): some changes to time-display-units

--- src/shared/utils/timeCycle/time-display-utils.ts
+++ src/shared/utils/timeCycle/time-display-utils.ts
@@ -5,10 +5,12 @@ const AM_PM_OUTPUT_TEMPLATE = "Time of Day: %02s:%02s %s";
 
 const OUTPUT_TIME_MULTIPLIER = 24 / MINUTES_PER_DAY;
 
-export function timeOfDayToText(displayMinutes: number, useAmPm = true) {
+export function timeOfDayToText(displayMinutes: number, useAmPm = false) {
 	displayMinutes = math.round(displayMinutes * OUTPUT_TIME_MULTIPLIER);
 
 	let displayHours = math.round(displayMinutes / 60) % 24;
+
+	displayMinutes %= 60;
 
 	if (!useAmPm) {
 		const text = DEFAULT_OUTPUT_TEMPLATE.format(displayHours, displayMinutes % 60);
@@ -19,8 +21,9 @@ export function timeOfDayToText(displayMinutes: number, useAmPm = true) {
 	const amPm = displayHours >= 12 ? "PM" : "AM";
 
 	displayHours %= 12;
+	displayHours = displayHours === 0 ? 12 : displayHours;
 
-	const text = AM_PM_OUTPUT_TEMPLATE.format(displayHours === 0 ? 12 : displayHours, displayMinutes % 60, amPm);
+	const text = AM_PM_OUTPUT_TEMPLATE.format(displayHours, displayMinutes % 60, amPm);
 
 	return text;
 }
