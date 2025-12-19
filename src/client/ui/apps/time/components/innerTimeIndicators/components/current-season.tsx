b603e957dcc675b96010cc8a3218ada93a1b678b
r0rnor <r0rnor@dev.com>
2025-12-19 16:03:14 +0100

    feat(ui): time indicator have selector, current season

--- src/client/ui/apps/time/components/innerTimeIndicators/components/current-season.tsx
+++ src/client/ui/apps/time/components/innerTimeIndicators/components/current-season.tsx
@@ -1,6 +1,12 @@
 import React from "@rbxts/react";
 import TimeIndicator from "./time-indicator";
+import { selectSeason } from "shared/store/selectors/timeCycleSelector";
 
 export default function CurrentSeason() {
-	return <TimeIndicator />;
+	return (
+		<TimeIndicator
+			selector={selectSeason}
+			toText={(value) => `Season: ${value}`}
+		/>
+	);
 }
