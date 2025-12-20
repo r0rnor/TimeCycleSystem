835d0a1866ac7c57f4ac1aea94c7e14afd90ad3a
r0rnor <r0rnor@dev.com>
2025-12-20 15:36:54 +0100

    feat(core): smooth animation of transition between night and day

--- src/server/services/TimeCycle/SunMotionService.ts
+++ src/server/services/TimeCycle/SunMotionService.ts
@@ -1,25 +1,39 @@
 import { Service, OnStart } from "@flamework/core";
-import { Lighting } from "@rbxts/services";
+import { Lighting, TweenService } from "@rbxts/services";
 import { store } from "server/store";
-import { DAY_CLOCK_TIME, NIGHT_CLOCK_TIME } from "shared/configs/timeCycle/ClockTime";
+import { DAY_CLOCK_TIME, NIGHT_CLOCK_TIME, SUN_MOVEMENT_TWEEN_INFO } from "shared/configs/timeCycle/ClockTime";
 import { selectDayOfYear, selectTimeOfDay } from "shared/store/selectors/timeCycleSelector";
 import { getSunriseTime, getSunsetTime, timeOfDaySecondsToClockTime } from "shared/utils/timeCycle/day-duration-utils";
 
 @Service({})
 export class SunMotionService implements OnStart {
+	private isMoving = false;
+
 	onStart() {
 		store.subscribe(selectTimeOfDay(), (timeOfDay) => {
 			this.updateSunPosition(timeOfDay);
 		});
 	}
 
-	private updateSunPosition(seconds: number) {
-		const clockTime = this.getClockTime(seconds);
-
-		Lighting.ClockTime = clockTime;
+	getIsMoving() {
+		return this.isMoving;
 	}
 
-	private getClockTime(seconds: number) {
+	private updateSunPosition(seconds: number) {
+		const currentClockTime = this.getCurrentClockTime();
+		const resultClockTime = this.getResultClockTime(seconds);
+		const isMoving = this.getIsMoving();
+
+		if (currentClockTime !== resultClockTime && !isMoving) {
+			this.setClockTime(resultClockTime);
+		}
+	}
+
+	private getCurrentClockTime() {
+		return Lighting.ClockTime;
+	}
+
+	private getResultClockTime(seconds: number) {
 		const dayOfYear = store.getState(selectDayOfYear());
 
 		const sunriseTime = getSunriseTime(dayOfYear);
@@ -30,5 +44,22 @@ export class SunMotionService implements OnStart {
 		const isDaytime = currentClockTime >= sunriseTime && currentClockTime < sunsetTime;
 
 		return isDaytime ? DAY_CLOCK_TIME : NIGHT_CLOCK_TIME;
+	}
+
+	private setClockTime(clockTime: number) {
+		this.setIsMoving(true);
+
+		const tween = TweenService.Create(Lighting, SUN_MOVEMENT_TWEEN_INFO, {
+			ClockTime: clockTime,
+		});
+
+		tween.Play();
+		tween.Completed.Connect(() => {
+			this.setIsMoving(false);
+		});
+	}
+
+	private setIsMoving(value: boolean) {
+		this.isMoving = value;
 	}
 }
--- src/shared/configs/timeCycle/ClockTime.ts
+++ src/shared/configs/timeCycle/ClockTime.ts
@@ -1,5 +1,7 @@
 export const SUNRISE_CLOCK_TIME = 8;
 export const SUNSET_CLOCK_TIME = 18;
 
-export const DAY_CLOCK_TIME = 12;
-export const NIGHT_CLOCK_TIME = 24;
+export const DAY_CLOCK_TIME = 13;
+export const NIGHT_CLOCK_TIME = 1;
+
+export const SUN_MOVEMENT_TWEEN_INFO = new TweenInfo(5, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut);
